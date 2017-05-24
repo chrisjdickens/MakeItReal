36ff9ac7c59b322f169612140d2b35e1f76bc573
chrisjdickens <christopherjdickens@hotmail.com>
2017-05-24 16:48:51 -0500

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -1,13 +1,12 @@
 require 'sinatra'
   
-  get "/" do
-   if params[:nombre] == "" || params[:nombre] == nil
-     "Hola desconocido!"
-   else
+  get "/makers/:nombre" do
+   
    	  <<-HTML 
    	  
    	  <h1> Hola #{params[:nombre]} </h1> 
 
    	  HTML
    end
-  end
+    
+  
